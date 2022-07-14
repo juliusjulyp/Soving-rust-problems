71960c84799aa4ac2777f22ffffe4a295dc16784
Julyp Julius <76010076+juliusjulyp@users.noreply.github.com>
2022-07-15 00:06:53 +0300

    Update varable1.rs

--- varable1.rs
+++ varable1.rs
@@ -7,3 +7,15 @@ fn main() {
     assert_eq!(x, y);
 // assert_eq! is used to check equality of two expressions
     println!("Success!");
+
+    
+    //second quiz
+    //mut is used for reassignment
+   fn main() {
+    let mut x =  1;
+       //x+2 adds one to the value of x
+    x += 2; 
+    
+    assert_eq!(x, 3);
+    println!("Success!");
+}
